31aaed76037c3d6e4c30f5de385fbba921c85646
Naveen Manjappa <NManjappa@manh.com>
2025-04-30 07:30:59 +0100

    Enhance project configuration and structure
    - Added Entity Framework Core Tools package reference to the API project.
    - Updated project references in the solution file for better organization.
    - Removed sensitive connection string from UaccDemoContext.
    - Updated Angular configuration to include assets from the public directory.
    - Added production environment configuration for Angular application.
    - Created web.config for Angular routing and security settings.

--- EnrollCourse.Core/Entities/UaccDemoContext.cs
+++ EnrollCourse.Core/Entities/UaccDemoContext.cs
@@ -39,10 +39,6 @@ public partial class UaccDemoContext : DbContext
 
     public virtual DbSet<VideoRequest> VideoRequests { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=SCALE-SQL2019-V1.manhdev.com;Initial Catalog=UACC_DEMO;User ID=manh;Password=Admin&*1234;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Course>(entity =>
